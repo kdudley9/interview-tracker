7e17662da025709e85e89a3f98d49a89a3f150f3
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2023-10-01 22:50:22 -0400

    Initial commit

---
+++ src/test/java/com/kennethdudley/interviewtracker/InterviewTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennethdudley.interviewtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
